e27e08ee3f5007cb3df08a734a57710776d208eb
Undo4 <78760229+Undo4@users.noreply.github.com>
2021-02-08 21:05:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+    <link rel="icon" href="images/logo.png" sizes="500x500" type="image/png">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Grafiki Store</title>
+	</head>
+	<body>
+<div id="navbar">
+    <a href="#" id="navtitle">Grafiki Store</a> 
+    <a href="#" style="margin-left:25px">Home</a>
+    <a href="#">Graficy</a>
+    <a href="#">Darmowe</a>
+    <a href="#">Współpraca</a>
+    <a href="#">Dotacje</a>
+</div>
+
+<script src="app.js"></script>
+    </body>
+    </html>
